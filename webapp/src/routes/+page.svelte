fba40c7c6fdf86899007ef548482a7a977c84620
Etai Nadler <75544136+etainad008@users.noreply.github.com>
2023-11-05 22:00:45 +0200

    Added color theme and css reset

--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -1,2 +1,36 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+    :global(:root[data-theme='light']) {
+        --text: #050406;
+        --background: #fdfcfd;
+        --primary: #614979;
+        --secondary: #dbd2e4;
+        --accent: #765893;
+    }
+
+    :global(:root[data-theme='dark']) {
+        --text: #faf9fb;
+        --background: #030203;
+        --primary: #9e86b6;
+        --secondary: #241b2d;
+        --accent: #8b6ca7;
+    }
+
+    :global(*, *::before, *::after) {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font: inherit;
+    }
+
+    :global(html) {
+        color-scheme: dark light;
+    }
+
+    :global(img, picture, svg, video) {
+        display: block;
+        max-width: 100%;
+    }
+</style>
